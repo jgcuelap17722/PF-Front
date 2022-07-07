5c5fe3724d04d56bdb5bfcf0d19f86cdedc089d1
S-alfaro <sebastianealfaro@gmail.com>
2022-07-07 19:13:18 -0300

    form fixed

--- src/components/Home/HomeSearcher.js
+++ src/components/Home/HomeSearcher.js
@@ -9,7 +9,7 @@ const HomeSearcher = () => {
         <input className={s.pet} type="text" placeholder='Mascota'/>
         <div className={s.line}></div>
         <input className={s.city} type="text" placeholder='Ciudad'/>
-        {/* <input type="button" className={s.button}/> */}
+        <input type="button" className={s.button}/>
       </form>
     </div>
   )
