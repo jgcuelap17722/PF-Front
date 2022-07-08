a22254cd47410446bfcaa551e080cb1795be9b36
emiandd <emilandresaravena@icloud.com>
2022-07-08 01:02:54 -0400

    route: register created

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import Home from './components/Home';
 import { Route, Routes } from 'react-router-dom';
 import PetCare from './components/PetCare/PetCare';
 
+import SignUp from './components/SignUp/SignUp';
+
 function App() {
   return (
     <div>
       <Routes>
         <Route path='/' element={<Home />} /> 
         {/* <Route path='/about-us' element={<AboutUs />} /> */}
-        {/* <Route path='/register' element={<Register />} /> */}
+        <Route path='/register' element={<SignUp />} />
         {/* <Route path='/login' element={<Login  />} /> */}
         {/* <Route path='/searcher' element={<Searcher />} /> */}
         <Route path='/pet-care' element={<PetCare />} />
