092ea56260f110fc8b78a11395065036157b8fa7
emiandd <emilandresaravena@icloud.com>
2022-07-09 12:09:49 -0400

    routes: create pet added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import PetCare from './components/PetCare/PetCare';
 import Login from './components/Login/Login';
 
 import PetDetail from './components/PetDetail/PetDetail';
-
+import CreatePet from './components/CreatePet/CreatePet';
 
 
 
@@ -14,12 +14,13 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} /> 
         {/* <Route path='/about-us' element={<AboutUs />} /> */}
-        {/* <Route path='/register' element={<Register />} /> */}
+        {/*<Route path='/register' element={<Register />} />*/}
         <Route path='/login' element={<Login  />} />
         {/* <Route path='/searcher' element={<Searcher />} /> */}
         <Route path='/pet-care' element={<PetCare />} />
         <Route path='/pet-detail' element={<PetDetail />} /> 
         {/* <Route path='/user' element={<User />} /> */}
+        <Route path='/create-pet' element={<CreatePet />} /> 
       </Routes>
     </div>
   );
