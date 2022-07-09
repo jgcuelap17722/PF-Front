9b61099ee9fac5f1ebb83136fd04d2e637c6f222
John Arango <johnj42@gmail.com>
2022-07-09 10:58:10 -0500

    Redux Fixed

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
 const { createStore, applyMiddleware } = require("redux");
 const { composeWithDevTools } = require("redux-devtools-extension");
-const thunk = require("redux-thunk");
-const reducer = require("./reducer");
+const {default: thunk} = require("redux-thunk");
+const {default: reducer} = require("./reducer");
 
 const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)))
 
