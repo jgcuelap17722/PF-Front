fdc544941a1c9517a2f60c3e56b031df886d4b37
S-alfaro <sebastianealfaro@gmail.com>
2022-07-09 13:04:40 -0300

    Check posible error

---
+++ src/components/Searcher/FilterOption.js
@@ -0,0 +1,16 @@
+import s from '../../css/FilterOption.module.css'
+
+const FilterOption = () => {
+  return (
+    <div>
+      <label htmlFor="">Raza</label>
+      <select name="" id="">
+        <option value=""></option>
+        <option value=""></option>
+        <option value=""></option>
+      </select>
+    </div>
+  )
+}
+
+export default FilterOption
--- src/components/Searcher/SearcherFilter.js
+++ src/components/Searcher/SearcherFilter.js
@@ -1,8 +1,11 @@
 import s from '../../css/SearcherFilter.module.css'
+import FilterOption from './FilterOption'
 
 const SearcherFilter = () => {
   return (
-    <aside className={s.searcherFilterBox}>SearcherFilter</aside>
+    <aside className={s.searcherFilterBox}>SearcherFilter
+      <FilterOption />
+    </aside>
   )
 }
 
