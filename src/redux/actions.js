ae6a0696ec130998b5adfd395424662c64467e92
emiandd <emilandresaravena@icloud.com>
2022-07-10 14:57:33 -0400

    POST USER: working

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,5 +1,9 @@
 import axios from 'axios'
 import { InfoApi } from "../assets/dataMockups/InfoApi.js";
+export const GET_COUNTRIES = 'GET_COUNTRIES';
+export const GET_CITIES_BY_COUNTRY = 'GET_CITIES_BY_COUNTRY';
+export const CREATE_NEW_USER = 'CREATE_NEW_USER';
+export const RESET_NEW_USER = 'RESET_NEW_USER';
 
 export  function getAllPets(){
         return async function (dispatch){
@@ -11,12 +15,65 @@ export  function getAllPets(){
         }
     }
 
-    export function getDetail(){
-        return async function (dispatch){
-            // var json = await axios.get('http://localhost:3001');
-            return dispatch({
-                type:'GET_DETAIL',
-                payload: InfoApi
-            })
-        }
+export function getDetail(){
+    return async function (dispatch){
+        // var json = await axios.get('http://localhost:3001');
+        return dispatch({
+            type:'GET_DETAIL',
+            payload: InfoApi
+        })
     }
+}
+
+export function getCountries(){
+
+    const url = 'http://localhost:5000/api/v1.0/countries';
+
+    return async function(dispatch){
+
+        return await fetch(url)
+            .then( response => response.json() )
+            .then( data => {
+                dispatch({type: GET_COUNTRIES, payload: data})
+            })
+    }
+}
+
+export function getCitiesByCountry(id){
+
+    const url = `http://localhost:5000/api/v1.0/cities/${id}`;
+
+    return async function(dispatch){
+
+        return await fetch(url)
+            .then( response => response.json() )
+            .then( data => {
+                dispatch({type: GET_CITIES_BY_COUNTRY, payload: data})
+            })
+
+    }
+
+}
+
+export function createNewUser(obj){
+
+    const url = 'http://localhost:5000/api/v1.0/user';
+    const options = {
+       method: 'POST',
+       headers: {'Content-Type' : 'Application/json'},
+        body: JSON.stringify(obj)
+    }
+
+    return async function(dispatch){
+
+        return await fetch(url, options)
+            .then( response => response.json() )
+            .then( data => {
+                dispatch({type: CREATE_NEW_USER, payload: data })
+            })
+    }
+}
+
+export function resetNewUser(){
+    return {type: RESET_NEW_USER, payload: {}}
+}
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,4 +1,7 @@
-import { } from './actions';
+import { GET_COUNTRIES,
+		 GET_CITIES_BY_COUNTRY,
+		 CREATE_NEW_USER,
+		 RESET_NEW_USER } from './actions';
 
 const initialState = {
 	allPets: [],
@@ -7,6 +10,8 @@ const initialState = {
 	newUser: {},
 	userDetail: {},
 	userLogged: {},
+	countries: [],
+	citiesByCountry: []
 };
 
 export default function reducer(state = initialState, action) {
@@ -22,6 +27,31 @@ export default function reducer(state = initialState, action) {
 				...state,
 				detail:action.payload,
 			}
+
+		case GET_COUNTRIES:
+			return{
+				...state,
+				countries: action.payload
+			}
+
+		case GET_CITIES_BY_COUNTRY:
+			return{
+				...state,
+				citiesByCountry: action.payload
+			}
+
+		case CREATE_NEW_USER:
+			return{
+				...state,
+				newUser: action.payload
+			}
+
+		case RESET_NEW_USER:
+			return{
+				...state,
+				newUser: action.payload
+			}			
+
 		default:
 			return state;
 	}
